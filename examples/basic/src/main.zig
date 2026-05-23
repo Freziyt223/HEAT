04bed76df9006608c1db21ac0c29327a40685662
Freziyt223 <freziyt223@gmail.com>
2026-05-23 20:58:21 +0300

    Some additions

--- examples/basic/src/main.zig
+++ examples/basic/src/main.zig
@@ -3,8 +3,7 @@ const Self = @This();
 const Engine = @import("Engine");
 const Conf = @import("Conf");
 
-pub fn init(this: *Self, args: std.process.Args) !void {
-    _ = this;
+pub fn init(args: std.process.Args) !void {
     _ = args;
     try Engine.IO.print("Hello, {s}\n", .{"world!"});
     const returned = try Engine.Async.call(Engine.IO.print, .{ "Hello, {s} From another thread!\n", .{"world!"} }, null);
@@ -13,6 +12,5 @@ pub fn init(this: *Self, args: std.process.Args) !void {
         std.debug.print("NOT OK!\n", .{});
     }
 }
-pub fn deinit(this: *Self) void {
-    _ = this;
-}
+
+pub fn deinit() void {}
